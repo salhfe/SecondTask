5533e48367dd7749f56d1bbad0ba21be8b7584e3
salhfe <salhfeplar@gmail.com>
2024-11-30 23:47:32 +0300

    f

---
+++ SecondTask/SecondTask.cpp
@@ -0,0 +1,48 @@
+﻿#include <cstdlib> 
+
+#include <ctime> 
+
+#include <algorithm>
+
+#include <array>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU"); // cout на русском
+
+
+    srand((unsigned)time(0)); // зерно рандома
+
+    int massive[11];
+
+    for (int i = 1; i <= 10; ++i)
+    {
+        int random = (rand() % 100) + 1;
+        massive[i] = random;
+    }
+
+    cout << "Элементы массива: ";
+
+    for (int i = 1; i <= 10; ++i)
+    {
+        cout << massive[i] << " ";
+    }
+
+    sort(begin(massive), end(massive));
+
+    cout << "\n\nСортировка массива: ";
+
+    for (int i = 1; i <= 10; ++i)
+    {
+        cout << massive[i] << " ";
+    }
+
+    int a;
+    cin >> a;
+
+    return 0;
+}
